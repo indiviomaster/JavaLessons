4ed3bdce6a515aaaf20d8e7a9e7e6c8657da387b
indivioM <indivio@yandex.ru>
2020-02-18 23:31:42 +0300

    v2

--- FirstClass.java
+++ FirstClass.java
@@ -1,9 +1,9 @@
-﻿public class FirstClass {
+public class FirstClass {
     public static void main(String[] args) {
         byte bt = 1;
         short st = 12;
         int it = 3232;
-        long lng = 3223156524;
+        long lng = 322315654;
 
         double db = mCalc(1,2,3,4);
         System.out.println(db);
