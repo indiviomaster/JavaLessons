bec0e7c61e127e5ec7ae7efae4295d6ced1bf9a1
indivioM <indivio@yandex.ru>
2020-03-02 23:50:36 +0300

    Signed-off-by: indivioM <indivio@yandex.ru>

--- Lesson4/Game.java
+++ Lesson4/Game.java
@@ -1,4 +1,3 @@
-package Lesson4;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,6 +7,7 @@ public class Game {
   private static int stepCounter = 0;
   private static final char DOT_O = 'O';
   private static final char DEFAULT = '_';
+  public static final int DOTS_TO_WIN = 4;
   private static final Random rnd = new Random();
 
   public static void main(String[] args) throws InterruptedException {
@@ -20,13 +20,12 @@ public class Game {
     while (isInGame) {
       System.out.println("Введите номер строки и номер столбца для хода");
       print(tab);
-      int x = in.nextInt();
-      int y = in.nextInt();
-      x--;
-      y--;
+      int x = in.nextInt()-1;
+      int y = in.nextInt()-1;
+
       if  (isValidStep(x, y, tab)) {
         tab[x][y] = DOT_X;
-        if (isWin(DOT_X, tab)) {
+        if (isWin(DOT_X, tab, DOTS_TO_WIN)) {
           print(tab);
           System.out.println("Вы победили");
           return;
@@ -37,14 +36,10 @@ public class Game {
           System.out.println("Ничья");
           return;
         }
-        movePC(tab);
-        System.out.println("Ход противника");
-        for (int i = 0; i < 7; i++) {
-          System.out.print(" * ");
-          Thread.sleep(500);
-        }
+        turnNPC(DOT_O, tab, DOTS_TO_WIN);
+
         System.out.println();
-        if (isWin(DOT_O, tab)) {
+        if (isWin(DOT_O, tab, DOTS_TO_WIN)) {
           print(tab);
           System.out.println("Вы проиграли");
           return;
@@ -53,24 +48,105 @@ public class Game {
     }
   }
 
-  private static boolean isWin(char ch, char[][] tab) {
+  private static boolean isWin(char ch, char[][] tab, int dotsToWin) {
     for (int i = 0; i < tab.length; i++) {
-      int sx = 0, sy = 0, d1 = 0, d2 = 0;
+      int sx = 0, sy = 0, d1 = 0, d2 = 0, d3 = 0;
       for (int j = 0; j < tab.length; j++) {
-        sx += tab[i][j] == ch ? 1 : 0;
-        sy += tab[j][i] == ch ? 1 : 0;
-        d1 += tab[j][j] == ch ? 1 : 0;
-        d2 += tab[j][tab.length - j - 1] == ch ? 1 : 0;
-        if (sx == tab.length || sy == tab.length || d1 == tab.length
-                || d2 == tab.length) {
-          return true;
-        }
+          if (tab[i][j] == ch){
+              sx += 1;
+          } else {
+              sx = 0;
+          }
+          if (tab[j][i] == ch){ sy += 1;
+          } else {
+              sy = 0;
+          }
+
+          if (tab[j][j] == ch){
+              d1 += 1;
+          } else {
+              d1 = 0;
+          }
+
+          if (tab[j][tab.length - j - 1] == ch){
+              d2 += 1;
+          } else {
+              d2 = 0;
+          }
+
+          if (sy == dotsToWin || sy == dotsToWin || d1 == dotsToWin || d2 == dotsToWin) {
+              return true;
+          }
       }
     }
+
+     for (int i = 0; i < tab.length; i++) {
+         int ddx = 0;
+         for (int j = i; j < (tab.length-i); j++){
+             if (tab[j][j - i] == ch){
+                 ddx += 1;
+             } else {
+                 ddx = 0;
+             }
+         }
+         if (ddx == dotsToWin) {
+             return true;
+         }
+     }
+
+      for (int i = 0; i < tab.length; i++) {
+          int ddx1 = 0;
+          for (int j = i+1; j < (tab.length-i-1); j++){
+              if (tab[j-1][j] == ch){
+                  ddx1 += 1;
+                  if (ddx1 == dotsToWin) {
+                      return true;
+                  }
+              } else {
+                  ddx1 = 0;
+              }
+
+          }
+
+      }
+
+      for (int i = 1; i < tab.length; i++) {
+          int ddy1 = 0;
+          for (int j = i; j < (tab.length-i); j++){
+              if (tab[j][tab.length - j ] == ch){
+                  ddy1 += 1;
+                  if (ddy1 == dotsToWin) {
+                      return true;
+                  }
+              } else {
+                  ddy1 = 0;
+              }
+
+          }
+
+      }
+
+      for (int i = 1; i < tab.length-2; i++) {
+          int ddy2 = 0;
+          for (int j = 0; j<=i; j++){
+              if (tab[j][i-j] == ch){
+                  ddy2 += 1;
+                  if (ddy2 == dotsToWin) {
+                      return true;
+                  }
+              } else {
+                  ddy2 = 0;
+              }
+
+          }
+
+      }
+
+
     return false;
   }
 
-  private static void movePC(char[][] tab) {
+  private static void turnNPC(char ch, char[][] tab, int dotsToWin) {
     while (true) {
       int x = rnd.nextInt(tab.length), y = rnd.nextInt(tab.length);
       if (isValidStep(x, y, tab)) {
