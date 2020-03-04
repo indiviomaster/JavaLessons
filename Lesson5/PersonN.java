b80c84ba23a2a075d80b76b8479de20b8a5c673b
indivioM <indivio@yandex.ru>
2020-03-04 22:43:55 +0300

    Employer add person file

---
+++ Lesson5/PersonN.java
@@ -0,0 +1,14 @@
+package Lesson5;
+
+public class PersonN {
+
+    protected String name, surname, lastname;
+    protected int age;
+
+    public PersonN(String surname, String name, String lastname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+}
